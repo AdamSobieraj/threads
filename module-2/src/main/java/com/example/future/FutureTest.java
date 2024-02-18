5f81b003accd2605c01a624335d3149992498bc4
AdamSobieraj <adam.sobieraj@op.pl>
2024-02-18 08:29:08 +0100

    main code

---
+++ module-2/src/main/java/com/example/future/FutureTest.java
@@ -0,0 +1,23 @@
+package com.example.future;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static com.example.util.SleepUtil.sleep;
+
+
+public class FutureTest {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Processor processor = new Processor();
+        final Future<Integer> futureResult = processor.process(5);
+
+        while (!futureResult.isDone()) {
+            System.out.println("Doing something else");
+            sleep(1);
+        }
+
+        final Integer result = futureResult.get();
+        System.out.println("Result " + result);
+    }
+}
---
+++ module-2/src/main/java/com/example/util/SleepUtil.java
@@ -0,0 +1,14 @@
+package com.example.util;
+
+public class SleepUtil {
+
+    public static void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
