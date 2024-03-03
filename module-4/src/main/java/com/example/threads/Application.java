457a3f1db45647a2749d52bd07229263be0cd0dc
AdamSobieraj <adam.sobieraj@op.pl>
2024-03-03 08:59:28 +0100

    main task

---
+++ module-4/src/main/java/com/example/threads/Application.java
@@ -0,0 +1,9 @@
+package com.example.threads;
+
+public class Application {
+
+    public static void main(String[] args) {
+        final ThreadCreator threadCreator = new ThreadCreator(1);
+        threadCreator.start();
+    }
+}
