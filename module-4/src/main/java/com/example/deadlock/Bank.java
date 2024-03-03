017b3dbd8f3f9b09f2f4ccaaee0e7f1a9c9744ce
AdamSobieraj <adam.sobieraj@op.pl>
2024-03-03 09:53:09 +0100

    Task 4 - 6

--- module-4/src/main/java/com/example/deadlock/Bank.java
+++ module-4/src/main/java/com/example/deadlock/Bank.java
@@ -1,13 +1,9 @@
 package com.example.deadlock;
 
 public class Bank {
-    private static final Object lock = new Object();
-
     public void transfer(BankAccount from, BankAccount to, double amount) {
-        synchronized (lock) {
-            from.withdraw(amount);
-            to.deposit(amount);
-        }
+        from.withdraw(amount);
+        to.deposit(amount);
     }
 }
 
