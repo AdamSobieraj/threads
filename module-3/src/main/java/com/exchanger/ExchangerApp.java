6ad4b047109210348149b61794da5d0cfe580bd1
AdamSobieraj <adam.sobieraj@op.pl>
2024-03-03 08:30:09 +0100

    main task

---
+++ module-3/src/main/java/com/exchanger/ExchangerApp.java
@@ -0,0 +1,16 @@
+package com.exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class ExchangerApp {
+
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<>();
+
+        new ExchangerConsumer(exchanger);
+        new ExchangerProducer(exchanger);
+    }
+}
+
+
+
